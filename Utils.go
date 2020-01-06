d30bcfde1c212215f50d043113bf0cd917b3623b
AllenDang <allengnr@gmail.com>
2020-01-06 16:25:43 +0800

    Add ToVec4Color.

--- Utils.go
+++ Utils.go
@@ -2,9 +2,12 @@ package giu
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"os"
+
+	"github.com/AllenDang/giu/imgui"
 )
 
 func LoadImage(imgPath string) (*image.RGBA, error) {
@@ -26,5 +29,14 @@ func LoadImage(imgPath string) (*image.RGBA, error) {
 		rgba := image.NewRGBA(trueImg.Bounds())
 		draw.Draw(rgba, trueImg.Bounds(), trueImg, image.Pt(0, 0), draw.Src)
 		return rgba, nil
+	}
+}
+
+func ToVec4Color(col color.RGBA) imgui.Vec4 {
+	return imgui.Vec4{
+		X: float32(col.R) / 255,
+		Y: float32(col.G) / 255,
+		Z: float32(col.B) / 255,
+		W: float32(col.A) / 255,
 	}
 }
---
+++ examples/setstyle/setstyle.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/AllenDang/giu"
+	"github.com/AllenDang/giu/imgui"
+)
+
+func loop(w *giu.MasterWindow) {
+	col := color.RGBA{0x36, 0x74, 0xD5, 255}
+
+	giu.SingleWindow(w, "set style", giu.Layout{
+		giu.Custom(func() {
+			// #3674D5
+			imgui.PushStyleColor(imgui.StyleColorText, giu.ToVec4Color(col))
+		}),
+		giu.Label("I'm a styled label setting by imgui.PushStyleColor"),
+		giu.Custom(func() {
+			imgui.PopStyleColor()
+		}),
+		giu.Label("I'm a normal label"),
+	})
+}
+
+func main() {
+	wnd := giu.NewMasterWindow("Set Style", 400, 200, false, nil)
+	wnd.Main(loop)
+}
