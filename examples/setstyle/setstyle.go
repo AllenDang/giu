07acb1f010fe5011d95397cae35e5e0f2797bdf9
AllenDang <allengnr@gmail.com>
2020-01-10 19:57:44 +0800

    Inline the color definition.

--- examples/setstyle/setstyle.go
+++ examples/setstyle/setstyle.go
@@ -8,11 +8,10 @@ import (
 )
 
 func loop(w *giu.MasterWindow) {
-	col := color.RGBA{0x36, 0x74, 0xD5, 255}
-
 	giu.SingleWindow(w, "set style", giu.Layout{
 		giu.Custom(func() {
 			// #3674D5
+			col := color.RGBA{0x36, 0x74, 0xD5, 255}
 			imgui.PushStyleColor(imgui.StyleColorText, giu.ToVec4Color(col))
 		}),
 		giu.Label("I'm a styled label setting by imgui.PushStyleColor"),
