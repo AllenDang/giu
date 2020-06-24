fa0e808b10ae1e839d7db1b431f869749954a6fc
AllenDang <allengnr@gmail.com>
2020-06-24 12:22:59 +0800

    Add MainMenuBar to multiwindow demo.

--- examples/multiwindow/multiwindow.go
+++ examples/multiwindow/multiwindow.go
@@ -6,6 +6,7 @@ import (
 
 var (
 	showWindow2 bool
+	checked     bool
 )
 
 func onShowWindow2() {
@@ -17,13 +18,25 @@ func onHideWindow2() {
 }
 
 func loop() {
-	g.Window("Window 1", 10, 10, 200, 100, g.Layout{
+	g.MainMenuBar(g.Layout{
+		g.Menu("File", g.Layout{
+			g.MenuItem("Open", nil),
+			g.Separator(),
+			g.MenuItem("Exit", nil),
+		}),
+		g.Menu("Misc", g.Layout{
+			g.Checkbox("Enable Me", &checked, nil),
+			g.Button("Button", nil),
+		}),
+	}).Build()
+
+	g.Window("Window 1", 10, 30, 200, 100, g.Layout{
 		g.Label("I'm a label in window 1"),
 		g.Button("Show Window 2", onShowWindow2),
 	})
 
 	if showWindow2 {
-		g.WindowV("Window 2", &showWindow2, g.WindowFlagsNone, 250, 10, 200, 100, g.Layout{
+		g.WindowV("Window 2", &showWindow2, g.WindowFlagsNone, 250, 30, 200, 100, g.Layout{
 			g.Label("I'm a label in window 2"),
 			g.Button("Hide me", onHideWindow2),
 		})
@@ -34,4 +47,3 @@ func main() {
 	wnd := g.NewMasterWindow("Multi sub window demo", 600, 400, 0, nil)
 	wnd.Main(loop)
 }
-
