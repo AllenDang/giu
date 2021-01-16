98573db688bb981afbf9b000689dce792bd7c344
Ian Ling <ian@iancaling.com>
2021-01-16 03:33:52 -0800

    Open PopupModal on Build

--- Widgets.go
+++ Widgets.go
@@ -1241,6 +1241,10 @@ func (p *PopupModalWidget) Layout(layout Layout) *PopupModalWidget {
 }
 
 func (p *PopupModalWidget) Build() {
+	if *p.open {
+		imgui.OpenPopup(p.name)
+	}
+
 	if imgui.BeginPopupModalV(p.name, p.open, p.flags) {
 		if p.layout != nil {
 			Update()
