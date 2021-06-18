8993ceb378e8c4571ab28a5d4608491a56d54a4a
AllenDang <allengnr@gmail.com>
2021-06-18 15:51:30 +0800

    Fix build failure and refine WindowShortcut demo code.

--- examples/keyboardShortcuts/keyboardShortcuts.go
+++ examples/keyboardShortcuts/keyboardShortcuts.go
@@ -9,9 +9,9 @@ var (
 
 func loop() {
 	giu.Window("Window 2").
-		RegisterKeyboardShortcuts([]giu.WindowShortcut{
-			{giu.KeyZ, giu.ModControl, func() { checkbox2 = !checkbox2 }},
-		}...).
+		RegisterKeyboardShortcuts(
+			giu.WindowShortcut{Key: giu.KeyZ, Modifier: giu.ModControl, Callback: func() { checkbox2 = !checkbox2 }},
+		).
 		Layout(
 			giu.Checkbox("Press Ctrl+C to change my state - I'm a global shortcut", &checkbox1),
 			giu.Checkbox("Press Ctrl+Z to change my state - I'm a local shortcut", &checkbox2),
@@ -25,9 +25,12 @@ func loop() {
 
 func main() {
 	wnd := giu.NewMasterWindow("keyboard shortcuts", 640, 480, 0).
-		RegisterKeyboardShortcuts([]giu.WindowShortcut{
-			{giu.KeyC, giu.ModControl, func() { checkbox1 = !checkbox1 }},
-		}...)
+		RegisterKeyboardShortcuts(
+			giu.WindowShortcut{
+				Key:      giu.KeyC,
+				Modifier: giu.ModControl,
+				Callback: func() { checkbox1 = !checkbox1 }},
+		)
 
 	wnd.Run(loop)
 }
