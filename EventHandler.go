3bb3929a30d5fdcb4a3130c6fa6bd2dd132c58a7
gucio321 <gucio321@protonmail.com>
2021-08-25 19:50:14 +0200

    eventHandler: add Key events

--- EventHandler.go
+++ EventHandler.go
@@ -6,16 +6,24 @@ type mouseEvent struct {
 	cond        func(MouseButton) bool
 }
 
+type keyEvent struct {
+	key      Key
+	callback func()
+	cond     func(Key) bool
+}
+
 // EventHandler is a universal event handler for giu widgets.
 // put giu.Event()... after any widget to handle any event
 type EventHandler struct {
 	hover       func()
 	mouseEvents []mouseEvent
+	keyEvents   []keyEvent
 }
 
 func Event() *EventHandler {
 	return &EventHandler{
 		mouseEvents: make([]mouseEvent, 0),
+		keyEvents:   make([]keyEvent, 0),
 	}
 }
 
@@ -23,6 +31,25 @@ func (eh *EventHandler) OnHover(onHover func()) *EventHandler {
 	eh.hover = onHover
 	return eh
 }
+
+// Key events
+
+func (eh *EventHandler) OnKeyDown(key Key, cb func()) *EventHandler {
+	eh.keyEvents = append(eh.keyEvents, keyEvent{key, cb, IsKeyDown})
+	return eh
+}
+
+func (eh *EventHandler) OnKeyPressed(key Key, cb func()) *EventHandler {
+	eh.keyEvents = append(eh.keyEvents, keyEvent{key, cb, IsKeyPressed})
+	return eh
+}
+
+func (eh *EventHandler) OnKeyReleased(key Key, cb func()) *EventHandler {
+	eh.keyEvents = append(eh.keyEvents, keyEvent{key, cb, IsKeyReleased})
+	return eh
+}
+
+// Mouse events
 
 func (eh *EventHandler) OnClick(mouseButton MouseButton, callback func()) *EventHandler {
 	eh.mouseEvents = append(eh.mouseEvents, mouseEvent{mouseButton, callback, IsMouseClicked})
@@ -51,6 +78,14 @@ func (eh *EventHandler) Build() {
 
 	if eh.hover != nil {
 		eh.hover()
+	}
+
+	if len(eh.keyEvents) > 0 {
+		for _, event := range eh.keyEvents {
+			if event.callback != nil && event.cond(event.key) {
+				event.callback()
+			}
+		}
 	}
 
 	if len(eh.mouseEvents) > 0 {
