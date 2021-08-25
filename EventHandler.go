8d8b82355b7ee839610b4fda297a3860d641f7e8
gucio321 <gucio321@protonmail.com>
2021-08-25 19:29:39 +0200

    eventHandler: add OnMouseDown

--- EventHandler.go
+++ EventHandler.go
@@ -8,15 +8,17 @@ type mouseEvent struct {
 // EventHandler is a universal event handler for giu widgets.
 // put giu.Event()... after any widget to handle any event
 type EventHandler struct {
-	hover  func()
-	click  []mouseEvent
-	dClick []mouseEvent
+	hover     func()
+	click     []mouseEvent
+	dClick    []mouseEvent
+	mouseDown []mouseEvent
 }
 
 func Event() *EventHandler {
 	return &EventHandler{
-		click:  make([]mouseEvent, 0),
-		dClick: make([]mouseEvent, 0),
+		click:     make([]mouseEvent, 0),
+		dClick:    make([]mouseEvent, 0),
+		mouseDown: make([]mouseEvent, 0),
 	}
 }
 
@@ -35,12 +37,19 @@ func (eh *EventHandler) OnDClick(mouseButton MouseButton, callback func()) *Even
 	return eh
 }
 
+func (eh *EventHandler) OnMouseDown(mouseButton MouseButton, callback func()) *EventHandler {
+	eh.mouseDown = append(eh.mouseDown, mouseEvent{mouseButton, callback})
+	return eh
+}
+
 func (eh *EventHandler) Build() {
-	isHovered := IsItemHovered()
+	if !IsItemHovered() {
+		return
+	}
 
 	if len(eh.click) > 0 {
 		for _, event := range eh.click {
-			if event.callback != nil && isHovered && IsMouseClicked(event.mouseButton) {
+			if event.callback != nil && IsMouseClicked(event.mouseButton) {
 				event.callback()
 			}
 		}
@@ -48,7 +57,15 @@ func (eh *EventHandler) Build() {
 
 	if len(eh.dClick) > 0 {
 		for _, event := range eh.dClick {
-			if event.callback != nil && isHovered && IsMouseDoubleClicked(event.mouseButton) {
+			if event.callback != nil && IsMouseDoubleClicked(event.mouseButton) {
+				event.callback()
+			}
+		}
+	}
+
+	if len(eh.mouseDown) > 0 {
+		for _, event := range eh.mouseDown {
+			if event.callback != nil && IsMouseDoubleClicked(event.mouseButton) {
 				event.callback()
 			}
 		}
