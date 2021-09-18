cd7cd6b7847155f52361e521cb1f945eaa0a107b
gucio321 <gucio321@protonmail.com>
2021-09-18 19:11:46 +0200

    gmdeploy: lintfix

--- cmd/gmdeploy/utils.go
+++ cmd/gmdeploy/utils.go
@@ -7,23 +7,22 @@ import (
 	"os/exec"
 )
 
-func Save(name, data string) {
-	err := ioutil.WriteFile(name, []byte(data), 0o644)
-	if err != nil {
+func save(name, data string) {
+	const newFileMode = 0o644
+	if err := ioutil.WriteFile(name, []byte(data), newFileMode); err != nil {
 		log.Fatalf("Failed to save %s:%v\n", name, err)
 	}
 }
 
-func MkdirAll(name string) {
-	err := os.MkdirAll(name, 0o755)
-	if err != nil {
+func mkdirAll(name string) {
+	const newDirMode = 0o755
+	if err := os.MkdirAll(name, newDirMode); err != nil {
 		log.Fatalf("Failed to make all dir, %s:%v\n", name, err)
 	}
 }
 
-func RunCmd(cmd *exec.Cmd) {
-	err := cmd.Run()
-	if err != nil {
+func runCmd(cmd *exec.Cmd) {
+	if err := cmd.Run(); err != nil {
 		log.Fatalf("Failed to execute command:%s with error %v\n", cmd.String(), err)
 	}
 }
