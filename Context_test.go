50c8760f9ddd20b101dced4dffbd7194fb91f7ec
gucio321 <gucio321@protonmail.com>
2021-10-12 17:47:26 +0200

    add test for context.go

--- Context_test.go
+++ Context_test.go
@@ -31,6 +31,30 @@ func Test_SetGetState(t *testing.T) {
 	}
 }
 
+func Test_invalidState(t *testing.T) {
+	ctx := context{}
+
+	state1ID := "state1"
+	state2ID := "state2"
+	states := map[string]Disposable{
+		state1ID: &teststate{},
+		state2ID: &teststate{},
+	}
+
+	for i, s := range states {
+		ctx.SetState(i, s)
+	}
+
+	ctx.invalidAllState()
+
+	_ = ctx.GetState(state2ID)
+
+	ctx.cleanState()
+
+	assert.NotNil(t, ctx.GetState(state2ID), "altought state has been accessed during the frame, it has ben deleted by invalidAllState/cleanState")
+	assert.Nil(t, ctx.GetState(state1ID), "altought state hasn't been accessed during the frame, it hasn't ben deleted by invalidAllState/cleanState")
+}
+
 func Test_GetWidgetIndex(t *testing.T) {
 	ctx := context{}
 	for i := 0; i <= 3; i++ {
