b0e542f809492d75d4c1bb31615b534c1a281950
gucio321 <gucio321@protonmail.com>
2021-10-25 21:15:58 +0200

    add support for links (use with gucio321/imgui-go@69dc4f2)

--- examples/markdown/markdown.go
+++ examples/markdown/markdown.go
@@ -3,20 +3,24 @@ package main
 import (
 	"fmt"
 
+	"github.com/pkg/browser"
+
 	"github.com/AllenDang/giu"
 	"github.com/AllenDang/imgui-go"
 )
 
 var markdown string = fmt.Sprint(
 	"  * list\n",
-	"[link](https://github.com)\n",
+	"Here is [a link to some cool website!](https://github.com/AllenDang/giu) you must click it!\n",
 )
 
 func loop() {
 	giu.SingleWindow().Layout(
 		giu.InputTextMultiline(&markdown),
 		giu.Custom(func() {
-			imgui.Markdown(&markdown)
+			imgui.Markdown(&markdown, func(s string) {
+				browser.OpenURL(s)
+			})
 		}),
 	)
 }
