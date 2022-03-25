51eb8df3cae55b05f5d6c68ee1df4758493be005
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-03-25 20:00:25 +0100

    SplitLayout: make `sashPos` argument a pointer that user could change as well as read its value - don't store it in state
    fix #485

--- examples/splitter/splitter.go
+++ examples/splitter/splitter.go
@@ -4,18 +4,25 @@ import (
 	g "github.com/AllenDang/giu"
 )
 
+var (
+	sashPos1 float32 = 200
+	sashPos2 float32 = 200
+	sashPos3 float32 = 200
+	sashPos4 float32 = 100
+)
+
 func loop() {
 	g.SingleWindow().Layout(
-		g.SplitLayout(g.DirectionHorizontal, 200,
+		g.SplitLayout(g.DirectionHorizontal, &sashPos1,
 			g.Layout{
 				g.Label("Left panel"),
 				g.Row(g.Button("Button1"), g.Button("Button2")),
 			},
-			g.SplitLayout(g.DirectionVertical, 200,
+			g.SplitLayout(g.DirectionVertical, &sashPos2,
 				g.Layout{},
-				g.SplitLayout(g.DirectionHorizontal, 200,
+				g.SplitLayout(g.DirectionHorizontal, &sashPos3,
 					g.Layout{},
-					g.SplitLayout(g.DirectionVertical, 100,
+					g.SplitLayout(g.DirectionVertical, &sashPos4,
 						g.Layout{},
 						g.Layout{},
 					),
