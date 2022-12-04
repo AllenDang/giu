94996978104e0d51752e2026dac4bbd1d76bdc56
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-12-04 12:24:06 +0100

    migration.sh: rename style vars

--- migration.sh
+++ migration.sh
@@ -1,4 +1,7 @@
+git stash
 files=`find . -iname \*go`
 sed -ie 's/imgui/cimgui/g' $files
 go get github.com/AllenDang/cimgui-go@158164eb30c79c00a3c393a1d6642609f2f2e206
 go mod tidy
+sed -ie 's/cimgui\.StyleColorID/cimgui\.ImGuiCol/g' $files
+sed -ie 's/cimgui\.StyleVarID/cimgui\.ImGuiStyleVar/g' $files
