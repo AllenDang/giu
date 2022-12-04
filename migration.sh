7afd070aed426a1167bddc6a9ee33e3fb871984c
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-12-04 14:08:06 +0100

    migration: update

--- migration.sh
+++ migration.sh
@@ -1,12 +1,18 @@
-git add migration.sh
+git add migration.*
 git commit --amend
 git stash
 files=`find . -iname \*go`
+
+# switch to cimgui-go
 sed -ie 's/imgui/cimgui/g' $files
 go get github.com/AllenDang/cimgui-go@158164eb30c79c00a3c393a1d6642609f2f2e206
 go mod tidy
+
+# mainly StyleIDs.go
 sed -ie 's/cimgui\.StyleColorID/cimgui\.ImGuiCol/g' $files
 sed -ie 's/cimgui\.StyleVarID/cimgui\.ImGuiStyleVar/g' $files
+
+# another types
 sed -ie 's/cimgui\.DrawList/cimgui\.ImDrawList/g' $files
 sed -ie 's/cimgui\.TextureID/cimgui\.ImTextureID/g' $files
 sed -ie 's/cimgui\.Vec2/cimgui\.ImVec2/g' $files
@@ -15,6 +21,8 @@ sed -ie 's/cimgui\.Font/cimgui\.ImFont/g' $files
 sed -ie 's/cimgui\.Condition/cimgui\.ImGuiCond/g' $files
 sed -ie 's/cimgui\.InputTextCallback/cimgui\.ImGuiInputTextCallback/g' $files
 sed -ie 's/cimgui\.Context/cimgui\.ImGuiContext/g' $files
+
+# Context; TODO - check if nothing else is changed
 sed -ie 's/cimgui\.IO()/cimgui\.GetIO()/g' $files
 sed -ie 's/cimgui\.IO/cimgui\.ImGuiIO/g' $files
 
@@ -22,18 +30,27 @@ sed -ie 's/cimgui\.IO/cimgui\.ImGuiIO/g' $files
 #
 # input text:
 sed -ie 's/cimgui\.InputTextFlags\(\w\+\)/cimgui\.ImGuiInputTextFlags_\1/g' $files
+# API CHANGE!
 sed -ie 's/^.*cimgui\.ImGuiInputTextFlags_AlwaysInsertMode.*//g' $files
+
 # window flags
-sed -ie 's/cimgui\.WindowFlags/cimgui\.GLFWWindowFlags/g' $files
-# type was int; change to cimgui.GLFWWindowFlags
+sed -ie 's/cimgui\.WindowFlags/cimgui\.ImGuiWindowFlags/g' $files
+# type was int; change to cimgui.ImGuiWindowFlags
 sed -ie 's/\(type WindowFlags \)int/\1cimgui.GLFWWindowFlags/g' $files
-sed -ie 's/\(cimgui\.GLFWWindowFlags\w\+\)/WindowFlags(\1)/g' $files
+sed -ie 's/\(cimgui\.ImGuiWindowFlags\)\(\w\+\)/WindowFlags(\1_\2)/g' $files
 # remove some flags as they are not present in cimgui-go
-sed -ie 's/^.*WindowFlagsNoTitleBar.*//g' Flags.go
-sed -ie 's/WindowFlagsNoResize/WindowFlagsNoResizable/g' Flags.go
-sed -ie 's/^.*WindowFlagsNoMove.*//g' Flags.go
-sed -ie 's/^.*WindowFlagsNoScrollbar.*//g' Flags.go
-sed -ie 's/^.*WindowFlagsNoScrollWithMouse.*//g' Flags.go
+# API CHANGE!
+#sed -ie 's/^.*WindowFlagsNoTitleBar.*//g' Flags.go
+#sed -ie 's/WindowFlagsNoResize/WindowFlagsNotResizable/g' Flags.go
+#sed -ie 's/^.*WindowFlagsNoMove.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsNoScrollbar.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsNoScrollWithMouse.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsNoCollapse.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsAlwaysAutoResize.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsNoBackground.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsNoSavedSettings.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsNoMouseInputs.*//g' Flags.go
+#sed -ie 's/^.*WindowFlagsMenuBar.*//g' Flags.go
 
 
 sed -ie 's/^/\/\/ /g' Markdown.go
