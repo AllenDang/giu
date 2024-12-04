0dbdb2f3a4b33653c14be4f6e9d9ee4ee3492079
gucio321 <gucio321@protonmail.com>
2024-12-04 07:39:25 +0100

    feat(SplitLayout): support different sashPos interpretations
    until now the only and default behaviour was counting from left/top.
    I've added possibility to interpret sashPos from right/bottom or as
    percent (%) of availableRegion

--- examples/splitter/splitter.go
+++ examples/splitter/splitter.go
@@ -7,8 +7,8 @@ import (
 
 var (
 	sashPos1 float32 = 200
-	sashPos2 float32 = 200
-	sashPos3 float32 = 200
+	sashPos2 float32 = 400
+	sashPos3 float32 = 0.5
 	sashPos4 float32 = 100
 )
 
@@ -18,17 +18,22 @@ func loop() {
 			g.Layout{
 				g.Label("Left panel"),
 				g.Row(g.Button("Button1"), g.Button("Button2")),
+				g.Label("Info: These SplitLayouts have different SplitRefTypes. Try to resize MasterWindow and see what happens."),
 			},
 			g.SplitLayout(g.DirectionVertical, &sashPos2,
-				g.Layout{},
+				g.Layout{
+					g.Label("This split is counted from right edge."),
+				},
 				g.SplitLayout(g.DirectionHorizontal, &sashPos3,
-					g.Layout{},
+					g.Layout{
+						g.Label("This starts from 50%"),
+					},
 					g.SplitLayout(g.DirectionVertical, &sashPos4,
 						g.Layout{},
 						g.Layout{},
 					),
-				),
-			),
+				).SplitRefType(g.SplitRefProc),
+			).SplitRefType(g.SplitRefRight),
 		),
 	)
 }
